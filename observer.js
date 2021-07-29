61a80942c8f0dcc3e3a87e7f7dbfa6fb84594bcf
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-07-29 18:13:37 +0100

    process event emitter

--- observer.js
+++ observer.js
@@ -18,3 +18,13 @@ jonBullion.emit('new album')
 jonBullion.emit('new album')
 jonBullion.emit('brag')
 
+process.on('exit', (code) => {
+	console.log("Node is done with file execution. Goodbye...", code)
+})
+process.on('beforeExit', (code) => {
+	console.log("Almost done...", code)
+})
+process.on('Error', (code) => {
+	console.log("e don happen, I don tell am...", code)
+})
+
