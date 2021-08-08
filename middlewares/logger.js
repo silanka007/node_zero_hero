7c162617cc70107007e07bfc45570aa876fd4f1f
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-08-08 19:10:22 +0100

    abstraction middlewares to folder

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,6 +1,8 @@
-module.exports.logger = (req, res, next) => {
+function logger(req, res, next) {
   const start = Date.now();
   next();
   const duration = Date.now() - start;
   console.log(`${req.method} - ${req.url} - ${res.statusCode} : ${duration}ms`);
-};
+}
+
+module.exports = { logger };
